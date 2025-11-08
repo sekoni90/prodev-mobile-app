42413553e4ec04c57dc8f931106f77e0ebe771b6
sekoni90 <sekonioyetunji@gmail.com>
2025-11-08 18:07:21 +0000

    initial commit

--- prodev-mobile-app-0x06/app/layout.tsx
+++ prodev-mobile-app-0x06/app/layout.tsx
@@ -1,38 +1,14 @@
-import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
+import { View } from 'react-native'
 
-const HomeRootLayout = () => {
+export default function RootLayout() {
   return (
-    <Tabs screenOptions={{
-        tabBarActiveTintColor: '#34967C',
-        headerShown: false
-    }} >
-      <Tabs.Screen name="index" options={{
-        title: 'Home',
-        tabBarIcon: ({ color }: { color: string }) => <AntDesign name="home" size={24} color={color} />,
-      }} />
-      <Tabs.Screen name="search" options={{
-        title: 'Search',
-        headerShown: true,
-        tabBarIcon: ({ color }: { color: string }) => <Feather name="search" size={24} color={color} />,
-      }} />
-      <Tabs.Screen name="saved" options={{
-        title: 'Saved',
-        headerShown: true,
-        tabBarIcon: ({ color }: { color: string }) => <EvilIcons name="heart" size={27} color={color} />
-      }} />
-        <Tabs.Screen name="inbox" options={{
-        title: 'Inbox',
-        headerShown: true,
-        tabBarIcon: ({ color }: { color: string }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
-      }} />
-        <Tabs.Screen name="profile" options={{
-        title: 'Profile',
-        headerShown: true,
-        tabBarIcon: ({ color }: { color: string }) => <FontAwesome name="user-o" size={24} color={color} />
-      }} />
-    </Tabs>
-  )
+    <Stack screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="/" />
+      <Stack.Screen name="join" />
+      <Stack.Screen name="signin" />
+    </Stack>
+  );
 }
-
-export default HomeRootLayout;
